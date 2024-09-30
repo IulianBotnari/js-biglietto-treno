85f6bda1896c63c71515ff1f7757536b84784095
IulianBotnari <iulianbotnari93@gmail.com>
2024-09-30 15:11:02 +0200

    Add var java

---
+++ script.js
@@ -0,0 +1,10 @@
+let age = (document.getElementById(age: Number):HTMLElement);
+let kilometer = (document.getElementById(kilometer: Number):HTMLElement);
+
+
+let priceForKm = 0.21;
+
+let minorDiscount = 100 / 100 * 20;
+
+let overDiscount = 100 / 100 * 20;
+
