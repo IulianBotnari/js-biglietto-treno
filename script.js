8d91defda40935bf3738d52af747ab7273516c2e
IulianBotnari <iulianbotnari93@gmail.com>
2024-09-30 15:27:22 +0200

    Test script

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let age = (document.getElementById(age: Number):HTMLElement);
-let kilometer = (document.getElementById(kilometer: Number):HTMLElement);
 
+let age = (document.getElementById(`age`).value);
+
+let kilometer = (document.getElementById(`kilometer`).value);
 
 let priceForKm = 0.21;
 
 let minorDiscount = 100 / 100 * 20;
 
 let overDiscount = 100 / 100 * 20;
+
+function sum() {
+
+    kilometer * priceForKm;
+
+    document.getElementById(`result`)
+    
+}
+
+let x = 1
+let y = 2
+
+console.log(x + y);
+
 
